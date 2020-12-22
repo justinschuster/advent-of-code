f704413f6e0d182df96efc0a4b6dfc6d96c63eb6
Justin Schuster <schujustin@gmail.com>
2020-12-22 16:08:38 -0500

    Better day1 solutions

--- 2020/day1/day1.py
+++ 2020/day1/day1.py
@@ -2,37 +2,28 @@
 # -*- coding: utf-8 -*-
 
 input_file = 'input.txt'
-target_sum = 2020
 
-def read_input(file_string):
-    f = open(file_string, "r")
-    input_arr = f.read().split("\n")
-    return input_arr
+data = list(map(int, open(input_file, 'r').readlines()))
 
-def type_handler(entry):
-    num = 0
-    try:
-        num = int(entry)
-    except ValueError:
-        num = int(float(entry)) 
-    return num
+def part_one(data):
+    seen = set()
+    for num1 in data:
+        num2 = 2020 - num1
+        if num2 in seen:
+            return num1 * num2
+        seen.add(num1)
 
-def number_finder(arr, target_sum):
-    arr_len = len(arr)
-    for i in range(arr_len):
-        if arr[i] == '':
-            break
-        num1 = type_handler(arr[i])
-        if num1 <= 2020:
-            for j in range(arr_len):
-                if arr[j] == '':
-                    break
-                num2 = type_handler(arr[j])
-                if num2 <= 2020:
-                    if num1 + num2 == 2020:
-                        print(num1 * num2)
-                        return
- 
+def part_two(data):
+    seen = set()
+    two_sum = []
+    for i in data:
+        for j in two_sum:
+            k = 2020 - i - j
+            if k in seen:
+                return i * j * k
+        seen.add(i)
+        two_sum.append(i)
 
-input_arr = read_input(input_file)
-number_finder(input_arr, target_sum)
+
+print(part_one(data))
+print(part_two(data))
